827235aac168520e877b982799bcf9b47b66171b
Brandon Chung <brandon.k.chung@gmail.com>
2022-05-08 13:57:47 -0500

    add import sorter

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,24 @@
 module.exports = {
+  // normally prettier-plugins work by default without specifying,
+  // but for pnpm, had to manually add plugin
+  // reference - https://github.com/trivago/prettier-plugin-sort-imports/issues/51#issuecomment-1018985805
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  // import sorting plugin - `@trivago/prettier-plugin-sort-imports`
+  // see more here: https://github.com/trivago/prettier-plugin-sort-imports
+  importOrder: [
+    // react
+    '^react$',
+
+    // any other third party packages
+    '<THIRD_PARTY_MODULES>',
+
+    // relative imports
+    '^[./]'
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+
+  // default prettier settings
   semi: true,
   trailingComma: 'none',
   singleQuote: true,
