c2638c07899dac75ea13b387eccd62890a87d05b
Brandon Chung <brandon.k.chung@gmail.com>
2022-05-08 12:24:12 -0500

    finish scaffold

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['kentcdodds', 'kentcdodds/react', 'prettier'],
+  extends: ['kentcdodds', 'prettier'],
   ignorePatterns: ['.eslintrc.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
